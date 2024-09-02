4ac0a2d96c266428d4bc3b684f020673b12e8825
BiplawSingh <biplawgaurav@gmail.com>
2024-09-02 22:39:48 +0530

    Segregated queues based on service

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -13,8 +13,16 @@ class BookingController {
 
   async sendMessageToQueue(req, res) {
     const channel = await createChannel();
-    const data = {message: 'Success'};
-    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+    const payload = {
+      data: {
+        subject: 'This is a notification from queue',
+        content: 'Some queue will subscribe this',
+        recipientEmail: 'email@xyz.com',
+        notificationTime: '2024-09-03T22:00:00'
+      },
+      service: 'CREATE_TICKET'
+    };
+    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
     return res.status(200).json({
         message: 'Successfully published the event'
     });
